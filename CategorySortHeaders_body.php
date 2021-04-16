e6772ab16eb5adf1f5850fd518b52e762b35cee7
Universal Omega <1010delta1010@gmail.com>
2021-04-16 22:58:46 +0000

    [CategorySortHeaders] Cleanup and convert to ExtensionRegistry
    Bug: T185488
    Change-Id: I519a810458abe347eb173b0ce5954bfdbc0228f2

--- CategorySortHeaders_body.php
+++ CategorySortHeaders_body.php
@@ -1,34 +1,72 @@
 <?php
-
 /**
  * Pretty much based on UppercaseCollation from core.
  *
  * Collation that is case-insensitive, and allows specify
  * custom 'first-character' headings for category pages.
  */
-class CustomHeaderCollation extends Collation {
 
-	// The basic idea is we store the sortkey as three parts A^B^C
-	// A is the capitalized first letter of the header it falls under.
-	// B is the is the header it falls under (In whatever case the user enters)
-	// C is the rest of the sortkey.
-	//
-	// The user enters something like [[category:some cat|^my header^foo]]
-	// which gets turned into "^my header^foo\n<page name>"
-	// which we turn into "M^my header^FOO\n<PAGE NAME>"
-	function getSortKey( $string ) {
-		global $wgContLang, $wgCategorySortHeaderAppendPageNameToKey;
-		// UppercaseCollation uses an EN lang object always instead of content lang.
-		// I'm not sure why. To me it makes more sense to use $wgContLang.
-		// There's minnor differences in some languages (like Turkish)
+use MediaWiki\MediaWikiServices;
+
+class CustomHeaderCollation extends Collation {
+	public static function onRegistration() {
+		global $wgCategoryCollation;
+
+		/**
+		 * We can't override this core variable with extension.json,
+		 * so we set it here instead.
+		 *
+		 * Note, this extension is incompatible with changing $wgCategoryCollation
+		 * in LocalSettings.php. It defines its own Collation here, which is
+		 * roughly equivalent to 'uppercase', and thus can't be used
+		 * with 'uca-default' or any other custom collation.
+		 */
+		$wgCategoryCollation = 'CustomHeaderCollation';
+	}
+
+	/**
+	 * @param string $collationName
+	 * @param mixed &$collationObject
+	 * @return bool
+	 */
+	public static function onCategorySortHeadersSetup( $collationName, &$collationObject ) {
+		if ( $collationName === 'CustomHeaderCollation' ) {
+			$collationObject = new self;
+
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * The basic idea is we store the sortkey as three parts A^B^C
+	 * A is the capitalized first letter of the header it falls under.
+	 * B is the is the header it falls under (In whatever case the user enters)
+	 * C is the rest of the sortkey.
+	 *
+	 * The user enters something like [[category:some cat|^my header^foo]]
+	 * which gets turned into "^my header^foo\n<page name>"
+	 * which we turn into "M^my header^FOO\n<PAGE NAME>"
+	 *
+	 * @param string $string
+	 * @return string
+	 */
+	public function getSortKey( $string ) {
+		global $wgCategorySortHeaderAppendPageNameToKey;
+
+		$contentLanguage = MediaWikiServices::getInstance()->getContentLanguage();
 
 		$matches = [];
 		if ( preg_match( '/^\^([^\n^]*)\^(.*)$/Ds', $string, $matches ) ) {
-			if ( $matches[1] === '' ) { $matches[1] = ' ';
+			if ( $matches[1] === '' ) {
+				$matches[1] = ' ';
 			}
-			$part1 = $wgContLang->firstChar( $wgContLang->uc( $matches[1] ) );
+
+			$part1 = $contentLanguage->firstChar( $contentLanguage->uc( $matches[1] ) );
 			$part2 = $matches[1];
 			$part3prefix = '';
+
 			if ( $wgCategorySortHeaderAppendPageNameToKey ) {
 				// This is kind of ugly, and seems wrong
 				// because it shouldn't be the collations
@@ -42,31 +80,35 @@ class CustomHeaderCollation extends Collation {
 					$part3prefix = $trimmed;
 				}
 			}
-			$part3 = $wgContLang->uc( $part3prefix . $matches[2] );
 
+			$part3 = $contentLanguage->uc( $part3prefix . $matches[2] );
 		} else {
 			// Ordinay sortkey, no header info.
-			$part3 = $wgContLang->uc( $string );
-			$part1 = $part2 = $wgContLang->firstChar( $part3 );
+			$part3 = $contentLanguage->uc( $string );
+			$part1 = $part2 = $contentLanguage->firstChar( $part3 );
 		}
+
 		return $part1 . '^' . $part2 . '^' . $part3;
 	}
 
-	function getFirstLetter( $string ) {
-			global $wgContLang;
+	/**
+	 * @param string $string
+	 * @return string
+	 */
+	public function getFirstLetter( $string ) {
+		# Stolen from UppercaseCollation
+		# not sure when this could actually happen.
+		if ( $string[0] === "\0" ) {
+			$string = substr( $string, 1 );
+		}
 
-			# Stolen from UppercaseCollation
-			# not sure when this could actually happen.
-			if ( $string[0] === "\0" ) {
-				$string = substr( $string, 1 );
-			}
+		$m = [];
+		if ( preg_match( '/^\^([^\n^]*)\^/', $string, $m ) ) {
+			return $m[1];
+		} else {
+			$contentLanguage = MediaWikiServices::getInstance()->getContentLanguage();
 
-			$m = [];
-			if ( preg_match( '/^\^([^\n^]*)\^/', $string, $m ) ) {
-				return $m[1];
-			} else {
-				// Probably shouldn't happen.
-				return $wgContLang->ucfirst( $wgContLang->firstChar( $string ) );
-			}
+			return $contentLanguage->ucfirst( $contentLanguage->firstChar( $string ) );
+		}
 	}
 }
